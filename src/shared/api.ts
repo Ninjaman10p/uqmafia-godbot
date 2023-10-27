cd6d77f7b016376e37089ef09f26b00f677c2bc5
Peter Gow <petergow@live.com.au>
2023-10-28 02:42:37 +1000

    add comments to messages

--- src/shared/api.ts
+++ src/shared/api.ts
@@ -1,5 +1,6 @@
 import { PlayerMessage } from "../server/role";
 
+// split this up over message types
 export interface PlayerClientMessage {
     userid: string; // should be stored in a cookie
 }
@@ -19,6 +20,7 @@ export interface JoinGameResponse {
     new_userid: string;
 }
 
+// split this one up too
 export interface AdminClientMessage {
     userid: string;
 }
